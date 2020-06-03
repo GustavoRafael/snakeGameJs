6cb2ae5a140b75b0c3ebc9b1ed2ebf33f731073b
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-03 20:36:20 -0300

    created the game.js file which stores the main functionalities of the game.

---
+++ game.js
@@ -0,0 +1,12 @@
+// this project is based on the snake game posted by - web dev simplified youtube channel
+
+let lastRenderTime = 0;
+
+function main(currentTime){
+    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
+    window.requestAnimationFrame(main);
+    lastRenderTime = currentTime;
+    console.log(secondsSinceLastRender);
+}
+
+window.requestAnimationFrame(main);
