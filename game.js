b4080ce1059bbb7636ee7c28d16d9eb8807a1f4f
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-03 20:53:28 -0300

    added the frame rate of the game and functions to update and draw

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
 // this project is based on the snake game posted by - web dev simplified youtube channel
 
 let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
 
 function main(currentTime){
-    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
     window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
+    if(secondsSinceLastRender < 1/SNAKE_SPEED) return
+
     lastRenderTime = currentTime;
-    console.log(secondsSinceLastRender);
+    console.log("Render!!");
+    
+    // update the game logic (eat the food, state: lost,playing, won ...)
+    update();
+    
+    // draw the elements of the game in the screen
+    draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update(){
+
+}
+function draw(){
+    
+}
