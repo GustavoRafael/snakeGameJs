fbd1c70231004a35838e195efb47550b9927b0fd
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-03 23:56:00 -0300

    added gameover check

--- game.js
+++ game.js
@@ -1,28 +1,52 @@
 // this project is based on the snake game posted by - web dev simplified youtube channel
+import { outSideGrid} from './grid.js'
+import {update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection , SNAKE_SPEED} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+
 
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+const gameBoard = document.getElementById('game-board');
+let gameOver = false;
 
 function main(currentTime){
+    
+    if(gameOver) {
+        if(confirm('You lost. Press ok to restart.')){
+            window.location = '/';
+        }
+        return
+    }
+
     window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000;
-    if(secondsSinceLastRender < 1/SNAKE_SPEED) return
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
     lastRenderTime = currentTime;
-    console.log("Render!!");
     
     // update the game logic (eat the food, state: lost,playing, won ...)
     update();
     
     // draw the elements of the game in the screen
     draw();
+    
 }
 
 window.requestAnimationFrame(main);
 
-function update(){
 
+function update() {
+    updateSnake();
+    updateFood();
+    checkDeath();
 }
-function draw(){
+
+function draw() {
     
+    // gameBoard.innerHTML = ''
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath(){
+    gameOver = outSideGrid(getSnakeHead()) || snakeIntersection()
 }
