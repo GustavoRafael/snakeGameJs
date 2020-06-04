14555a148251560df5d00dd1489066ce7c5b8151
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-03 23:56:41 -0300

    bug fixed

--- input.js
+++ input.js
@@ -1,5 +1,30 @@
 let inputDirection = {x: 0, y: 0};
+let lastInputDirection = {x: 0, y: 0};
+
+
+window.addEventListener('keydown', e => {
+    switch(e.key){
+        case 'ArrownUp':
+            if(lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: -1};
+            break;
+        case 'ArrownDown':
+            if(lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: 1};
+            break;
+        case 'ArrownLeft':
+            if(lastInputDirection.x !== 0) break
+            inputDirection = { x: -1, y: 0};
+            break;
+        case 'ArrownRight':
+            if(lastInputDirection.x !== 0) break
+            inputDirection = { x: 1, y: 0};
+            break;
+    }
+    
+});
 
 export function getInputDirection(){
+    lastInputDirection = inputDirection;
     return inputDirection;
 }
