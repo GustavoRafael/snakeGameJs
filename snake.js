c0d884f9a671c165e15a7cd10b40d87d67d91a5b
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-03 21:31:32 -0300

    added the body of the update function.

--- snake.js
+++ snake.js
@@ -8,15 +8,19 @@ const snakeBody = [
 ];
 
 export function update(){
-    console.log("update snake");
+    for(let i = snakeBody.length - 2; i>=0; i--){
+        snakeBody[i + 1] = {...snakeBody[i]};
+    }
 
+    snakeBody[0].x += 1;
+    snakeBody[0].y += 0;
 }
 
 export function draw(gameBoard){
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = segment.x;
-        snakeElement.style.gridColumnStar = segment.y;
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStar = segment.x;
         snakeElement.classList.add('snake');
         gameBoard.appendChild(snakeElement); 
     });
