0d0d4d49f890b1636c3fe7b8e6812454ebb7d860
Gustavo CR <gustavo.viagem@gmail.com>
2020-06-04 00:40:22 -0300

    found bug miss spelling

--- input.js
+++ input.js
@@ -3,26 +3,27 @@ let lastInputDirection = {x: 0, y: 0};
 
 
 window.addEventListener('keydown', e => {
+    console.log(e.key)
     switch(e.key){
-        case 'ArrownUp':
+        case 'ArrowUp':
             if(lastInputDirection.y !== 0) break
-            inputDirection = { x: 0, y: -1};
-            break;
-        case 'ArrownDown':
+            inputDirection = { x: 0, y: -1}
+            break
+        case 'ArrowDown':
             if(lastInputDirection.y !== 0) break
-            inputDirection = { x: 0, y: 1};
-            break;
-        case 'ArrownLeft':
+            inputDirection = { x: 0, y: 1}
+            break
+        case 'ArrowLeft':
             if(lastInputDirection.x !== 0) break
-            inputDirection = { x: -1, y: 0};
-            break;
-        case 'ArrownRight':
+            inputDirection = { x: -1, y: 0}
+            break
+        case 'ArrowRight':
             if(lastInputDirection.x !== 0) break
-            inputDirection = { x: 1, y: 0};
-            break;
+            inputDirection = { x: 1, y: 0}
+            break
     }
     
-});
+})
 
 export function getInputDirection(){
     lastInputDirection = inputDirection;
